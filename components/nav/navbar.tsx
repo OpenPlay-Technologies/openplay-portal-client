018f06173ca04e0248104010d51e7a0aad12097f
06 87 <0x0687.dev@gmail.com>
2025-03-31 09:05:47 +0200

    shadcn drawer test

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,20 +1,21 @@
 ﻿"use client";
 import Image from "next/image";
-import {cn} from "@/lib/utils";
-import {ArrowTopRightOnSquareIcon} from "@heroicons/react/24/outline";
-import React, {useEffect, useState} from "react";
+import { cn } from "@/lib/utils";
+// import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Wallet from "@/components/sui/wallet";
-import NavButton from "@/components/nav/nav-button";
-import {useTheme} from "next-themes";
+// import Wallet from "@/components/sui/wallet";
+// import NavButton from "@/components/nav/nav-button";
+import { useTheme } from "next-themes";
+import { DepositButton } from "./deposit-button";
 
 interface NavbarProps {
     transparent?: boolean;
 }
 
 const navigationLinks = [
-    {name: "Games", href: "/"},
-    {name: "Houses", href: "/houses"},
+    { name: "Games", href: "/" },
+    { name: "Houses", href: "/houses" },
     // {name: "Rewards", href: "/rewards"},
 ]
 
@@ -38,9 +39,9 @@ export default function Navbar(props: NavbarProps) {
                     <Link className={"flex-shrink-0"} href={"/"}>
                         {props.transparent || theme.theme == "dark" || (theme.theme == "system" && theme.systemTheme == "dark") ?
 
-                            <Image src={"/OpenPlay-Inverted-Color.png"} width={150} height={34} alt={"OpenPlay"}/>
+                            <Image src={"/OpenPlay-Inverted-Color.png"} width={150} height={34} alt={"OpenPlay"} />
                             :
-                            <Image src={"/OpenPlay Main Logo (2).png"} width={150} height={34} alt={"OpenPlay"}/>}
+                            <Image src={"/OpenPlay Main Logo (2).png"} width={150} height={34} alt={"OpenPlay"} />}
                     </Link>
             }
 
@@ -64,15 +65,16 @@ export default function Navbar(props: NavbarProps) {
             {/*Extra links*/}
             <div className={"flex flex-row gap-2 p-4"}>
                 {/*<ModeToggle/>*/}
-                <a href={process.env.NEXT_PUBLIC_GITBOOK_URL ?? ""} target={"_blank"} >
-                <NavButton
-                    tabIndex={0}
-                    text="Docs"
-                    icon={<ArrowTopRightOnSquareIcon className="w-6 h-6"/>}
-                    light={props.transparent}
-                />
-                </a>
-                <Wallet transparent={props.transparent}/>
+                {/* <a href={process.env.NEXT_PUBLIC_GITBOOK_URL ?? ""} target={"_blank"} >
+                    <NavButton
+                        tabIndex={0}
+                        text="Docs"
+                        icon={<ArrowTopRightOnSquareIcon className="w-6 h-6" />}
+                        light={props.transparent}
+                    />
+                </a> */}
+                <DepositButton balance={0.44} />
+                {/* <Wallet transparent={props.transparent} /> */}
             </div>
         </div>
     );
