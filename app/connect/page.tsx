c8de657622d41101ff7d40c40642383557aa46b8
06 87 <0x0687.dev@gmail.com>
2025-04-02 19:01:17 +0200

    fix suspense saerchparams

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,35 +1,10 @@
-// app/login/page.tsx
-'use client';
+import ConnectPage from "@/components/sui/connect-page";
+import { Suspense } from "react";
 
-import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { useWalletAuth } from '@/components/providers/wallet-auth-context-provider';
-import WalletConnectCard from '@/components/sui/wallet-connect-card';
-import { Loader } from '@/components/ui/loader';
-
-export default function LoginPage() {
-  const { openModal, isConnected, onWalletConnected, isLoading } = useWalletAuth();
-  const searchParams = useSearchParams();
-  const redirect = searchParams.get('redirect');
-
-  // Skip the page if the wallet is already connected
-  useEffect(() => {
-    if (isLoading) return; // Prevent running the effect if loading
-    if (isConnected) {
-      onWalletConnected(redirect || '/');
-    }
-  }, [openModal, onWalletConnected, redirect, isLoading, isConnected]);
-
+export default function Page() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      {isLoading || isConnected ? (
-        <Loader title='Attempting to connect to wallet...' />
-      ) 
-      : 
-      (
-        <WalletConnectCard onClick={() => openModal(redirect || '')} />
-      )}
-      
-    </div>
-  );
+    <Suspense>
+      <ConnectPage />
+    </Suspense>
+  )
 }
